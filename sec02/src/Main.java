6edecf1cb08e4ba3282853938af5132ee1b0fee0
eunbumkim <techcom@naver.com>
2023-06-10 01:08:02 +0900

    연산자

--- sec02/src/Main.java
+++ sec02/src/Main.java
@@ -1,5 +1,95 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        /**
+         *
+         *  연산자 (Operators)
+         *
+         * */
+
+
+        // 부호 연산자, +, -
+        int val = 100;
+        float real_val = 10.0F;
+
+        int res = -val;
+        float res_f = -real_val;
+
+        System.out.printf("val=%d, res=%d\n", val, res);                    // -를 한다고 해서 원래의 값 자체가 바뀌는것은 아님
+        System.out.printf("real_val=%f, res_f=%f\n", real_val, res_f);
+
+        System.out.println("--------------------------------------");
+        System.out.println("증감 연산자");
+        // 증감 연산자, ++, --
+        res = ++val;
+        res_f = ++real_val;
+        System.out.printf("res=%d, val=%d\n", res, val);
+        System.out.printf("res=%f, val=%f\n", res_f, real_val);
+
+        res = --val;
+        res_f = --real_val;
+        System.out.printf("res=%d, val=%d\n", res, val);
+        System.out.printf("res=%f, val=%f\n", res_f, real_val);
+
+        System.out.printf("val=%d\n", val++);   // 출력문이 선행된 후 val값이 증가된다.
+        System.out.printf("val=%d\n", --val);   // val값을 감소한 후 출력문이 실행된다.
+
+        System.out.println("--------------------------------------");
+        System.out.println("논리 부정 연산자");
+        // 논리 부정 연산자, !
+        boolean torf = true;
+        System.out.println("torf=" + torf);
+
+        torf = !torf;
+        System.out.println("torf=" + torf);
+
+        System.out.println("--------------------------------------");
+        System.out.println("사칙 연산자");
+        // 산술 연산자, +,-,*,/,%
+        res = val + 10; System.out.println("res = val + 10 ===> " + res);
+        res = val - 10; System.out.println("res = val - 10 ===> " + res);
+        res = val * 10; System.out.println("res = val * 10 ===> " + res);
+        res = val / 10; System.out.println("res = val / 10 ===> " + res);
+        res = val % 10; System.out.println("res = val % 10 ===> " + res);
+
+        res_f = real_val / 7;   System.out.println("실수의 나누기 연산 : " + res_f);
+        res_f = real_val % 7;   System.out.println("실수의 나머지 연산 : " + res_f);      // 실수형의 연산에도 나머지 연산은 수행됨
+
+        System.out.println("--------------------------------------");
+        System.out.println("대입 연산자");
+        // 대입 연산자, =, +=, -=, *=, /=, %=
+        res += 10; System.out.println("res += 10 ===> " + res);
+        res -= 5; System.out.println("res -= 10 ===> " + res);
+        res *= 5; System.out.println("res *= 10 ===> " + res);
+        res /= 5; System.out.println("res /= 10 ===> " + res);
+        res %= 5; System.out.println("res %= 10 ===> " + res);
+
+        System.out.println("--------------------------------------");
+        System.out.println("관계 연산자");
+        // 관계(비교) 연산자, <, <=, >, >=, ==, !=   , 결과값은 true or false
+        res = 10;   val = 5;
+        System.out.println("res > val : " + (res > val));  // true, 10 > 5
+        System.out.println("res >= val : " + (res >= val)); // true, 10 >= 5
+        System.out.println("res < val : " + (res < val));  // false, 10 < 5 ===> false
+        System.out.println("res <= val : " + (res <= val)); // false, 10 <= 5 ===> false
+        System.out.println("res == val : " + (res == val)); // false, 10 == 5 ===> false
+        System.out.println("res != val : " + (res != val)); // true, 10 != 5
+
+        System.out.println("--------------------------------------");
+        System.out.println("논리 연산자");
+        // 논리 연산자, &&(AND), ||(OR), ^(XOR), !(NOT)
+        boolean a = true, b = false;
+        System.out.println("a && b : " + (a && b));     // AND 연산
+        System.out.println("a || b : " + (a || b));     // OR 연산
+        System.out.println("a ^ b : " + (a ^ b));       // XOR 연산
+        System.out.println("!a : " + !a );              // NOT 연산
+
+        System.out.println("--------------------------------------");
+        System.out.println("삼항 연산자");
+
+        res = ( a && b) ? 10 : 100;
+        System.out.println("res = ( a && b) ? 10 : 100; ===> res = " + res );
     }
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("Hello Java!");
+
+        /**
+         * JAVA를 이용한 Application 개발
+         * 세계적으로 매우 많이 사용되는 프로그래밍 언어, 특히나 한국에서 더더욱..(관공서 웹서비스 백엔드 부분 개발에 많이 사용되었다)
+         * 모바일, 웹, 데스크탑, 게임 등등 장르 불문
+         *
+         * Java의 특징
+         * 각각 다른 플랫폼에서 동작이 가능하다
+         * 뼈속까지 OOP(Object Oriented Programming) Language 이다!!!
+         * C++, C#과 매우 유사하다.
+         *
+         * JAVA Installation
+         * JDK (Java Development Kit) : 자바 개발 라이브러리 & 컴파일러
+         * JRE (Java Runtime Environment) : 자바 실행 환경
+         * JVM (Java Virtual Machine) : 자바 가상 머신
+         * IDE (Integrated Development Environment) : Eclipse, Intelli J
+         * */
     }
 }
